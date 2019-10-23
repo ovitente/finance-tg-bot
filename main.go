b5242b367008d6b9226c9fb8ba08b59a0c4c953b
Ivan Kostrubin <ovitente@gmail.com>
2019-10-23 20:21:15 +0300

    tg bot work save

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
 	"gopkg.in/yaml.v2"
 )
 
@@ -24,7 +25,50 @@ type TgOpts struct {
 	BotId string `yaml:"bot_id"`
 }
 
-func main() {
+func telegramSendBack() {
+	// подключаемся к боту с помощью токена
+	bot, err := tgbotapi.NewBotAPI("ТОКЕН")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// инициализируем канал, куда будут прилетать обновления от API
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	err = bot.UpdatesChan(ucfg)
+	// читаем обновления из канала
+	for {
+		select {
+		case update := <-bot.Updates:
+			// Пользователь, который написал боту
+			UserName := update.Message.From.UserName
+
+			// ID чата/диалога.
+			// Может быть идентификатором как чата с пользователем
+			// (тогда он равен UserID) так и публичного чата/канала
+			ChatID := update.Message.Chat.ID
+
+			// Текст сообщения
+			Text := update.Message.Text
+
+			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+
+			// Ответим пользователю его же сообщением
+			reply := Text
+			// Созадаем сообщение
+			msg := tgbotapi.NewMessage(ChatID, reply)
+			// и отправляем его
+			bot.SendMessage(msg)
+		}
+
+	}
+}
+
+func appConf() *ConfigFile {
+
 	var confPath = "./config.yml"
 	var configFile = new(ConfigFile)
 
@@ -46,5 +90,11 @@ func main() {
 	fmt.Println(configFile.GoogleSheets.ID)
 	fmt.Println(configFile.Telegram.Token)
 	fmt.Println(configFile.Telegram.BotId)
+
+}
+
+func main() {
+	appConf()
+	telegramSendBack()
 
 }
