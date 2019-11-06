15b431ec1f344a1ea2daa7565dc003014baa4c04
Ivan Kostrubin <ovitente@gmail.com>
2019-11-06 13:56:29 +0200

    added token env

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
+	token := os.Getenv("TOKEN")
+
 	b, err := tb.NewBot(tb.Settings{
-		Token: "1009412817:AAFm0bzY2wtakQU63uJpM18DLUuQ_iSfIO8",
+		Token: token,
 		// You can also set custom API URL. If field is empty it equals to "https://api.telegram.org"
 		URL:    "https://api.telegram.org",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
